7e64da2a24267ce766df28acc173d356fcfb0035
Apprentice <apprentice@dbc-04.local>
2013-11-18 22:01:18 -0800

    worked with G

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dayanlist::Application.config.secret_token = 'df8edb88eb560ce99bcb281570bfddb3690fa65a9e8e6939f735478fd572821d258c2185e8020b4f4aa0320e92e5769fe3b20cd5b6a29182077ae291edeef353'
